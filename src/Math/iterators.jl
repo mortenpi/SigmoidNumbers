c755dd5a31a009db896247fbe43059a074e3231c
Morten Piibeleht <morten.piibeleht@gmail.com>
2019-05-09 11:07:18 +1200

    Deprecation: use new iteration protocol

--- src/Math/iterators.jl
+++ src/Math/iterators.jl
@@ -1,6 +1,6 @@
 #iterators.jl - creating iterator convenience types for SigmoidNumbers.
 
-import Base: next, start, done, size
+import Base: iterate, size
 
 increment(::Type{Sigmoid{N, ES, mode}}) where {N, ES, mode} = (@signbit) >> (N - 1)
 
@@ -14,12 +14,12 @@ end
   :(reinterpret(Sigmoid{N, ES, mode}, @u(x) - $inc))
 end
 
-start(T::Type{Sigmoid{N, ES, mode}}) where {N, ES, mode} = T(Inf)
-next(T::Type{Sigmoid{N, ES, mode}}, state) where {N, ES, mode} = (state, next(state))
-
-@generated function done(T::Type{Sigmoid{N, ES, mode}}, state) where {N, ES, mode}
-  last_element = reinterpret(Sigmoid{N, ES, mode}, (@signbit) - increment(Sigmoid{N, ES, mode}))
-  :(state == $last_element)
+@generated function iterate(T::Type{Sigmoid{N, ES, mode}}, state=T(Inf)) where {N, ES, mode}
+    last_element = reinterpret(Sigmoid{N, ES, mode}, (@signbit) - increment(Sigmoid{N, ES, mode}))
+    quote
+        state == $last_element && return nothing
+        (state, next(state))
+    end
 end
 
 size(T::Type{Sigmoid{N, ES, mode}}) where {N, ES, mode} = 1 << N
